c0e2ac20ab4126009629c0cedc1c71c8e97ce940
Kacper <kacperrymkiewicz@gmail.com>
2023-11-22 21:03:26 +0100

    Add camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float smoothValue;
+    Vector3 distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = target.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();   
+    }
+
+    void MoveCamera()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 targetPosition = target.position - distance;
+
+        transform.position = Vector3.Lerp(currentPosition, targetPosition, smoothValue * Time.deltaTime);
+    }
+}
