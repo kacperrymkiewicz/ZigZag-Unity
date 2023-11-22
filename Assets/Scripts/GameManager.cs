dacd13af22397e74f44c12c9e66708727b5831e9
Kacper <kacperrymkiewicz@gmail.com>
2023-11-22 17:50:55 +0100

    Add GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    public bool gameStarted;
+
+
+    private void Awake()
+    {
+        if(instance == null) 
+            instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameStart();
+            }
+        }   
+    }
+
+    public void GameStart()
+    {
+        gameStarted = true;
+    }
+
+    public void GameOver()
+    {
+
+    }
+}
